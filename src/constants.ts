c0c6deb9cba48df34933eb2c76f63486e9af11f8
ibeninc <royalrubee@gmail.com>
2022-05-01 22:37:42 +0100

    project setup

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0xb7d38B03dF6bA1DDB96222AEF2028f987a21B811'
+
+export const INIT_CODE_HASH = '0x07c5b8cafbf50136873a6f3976adbb4ece60e7ab1b445b0083b0ab4090df700e'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
